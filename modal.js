8b5ad33a02a224ee1fcbac1a772f1512b224297f
Annabel Rothschild <arothsch@wellesley.edu>
2017-11-12 11:28:01 -0500

    fixed resume link

--- modal.js
+++ modal.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 
     // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");

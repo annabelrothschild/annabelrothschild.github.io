c3283cc179fac54753043e62de92051b708f31f8
Annabel Rothschild <arothsch@wellesley.edu>
2018-10-07 21:36:45 -0400

    Update customjs.js

--- customjs.js
+++ customjs.js
@@ -1,7 +1,3 @@
-function show_about(){
-    document.getElementById('about').className.replace(" active", "");
-}
-
 function displayBio(){
     var artistSt = "Annabel Rothschild is a photographer from Atlanta, GA. Her work explores personal identity & narrative and subconscious/automatic production and has been selected to appear at SCAD Atlanta and the 79th Atlanta Dogwood Festival, among other venues. Her work has also appeared the national publication, Photographer’s Forum Best of Photography 2016, The Blue Rider literary magazine, and received several awards, including three Silver keys, from the Scholastic Art Awards."
     if (document.getElementById("statement").textContent.length < 1){

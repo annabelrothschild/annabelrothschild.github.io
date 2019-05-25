d7595625154e7e9f309bff248e6cb8f0fe0df935
Annabel Rothschild <arothsch@wellesley.edu>
2019-05-25 14:58:52 -0400

    fixing jekyll

---
+++ _site/old website bits/README.md
@@ -0,0 +1,3 @@
+# Personal Website
+
+annabelrothschild.space
---
+++ _site/old website bits/carouselSlide.js
@@ -0,0 +1,8 @@
+   $(document).bind('keyup', function(e) {
+        if(e.which == 39){
+            $('.carousel').carousel('next');
+        }
+        else if(e.which == 37){
+            $('.carousel').carousel('prev');
+        }
+    });

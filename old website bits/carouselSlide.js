a519adf36fbf8799be6daf179405770515f0dbd6
Annabel Rothschild <arothsch@wellesley.edu>
2020-06-05 11:51:48 -0400

    autoupdate

--- old website bits/carouselSlide.js
+++
@@ -1,8 +0,0 @@
-   $(document).bind('keyup', function(e) {
-        if(e.which == 39){
-            $('.carousel').carousel('next');
-        }
-        else if(e.which == 37){
-            $('.carousel').carousel('prev');
-        }
-    });
